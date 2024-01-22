09bbe2866f0ea925c22cab3481344218fe2b29c0
ysolarh <ysolarh@gmail.com>
2024-01-22 18:09:33 +0900

    Feat: add order by date

--- app.py
+++ app.py
@@ -23,10 +23,11 @@ class Application:
 app = Flask(__name__)
 db_module = DatabaseModule()
 cur = db_module.cur
-sql_item = "SELECT I.item_id AS id, C.name AS category, B.brand_name AS brand, I.product AS product, I.price AS price, I.url AS url, I.date AS date, I.sold AS sold \
+sql_item = ("SELECT I.item_id AS id, C.name AS category, B.brand_name AS brand, I.product AS product, I.price AS price, I.url AS url, I.date AS date, I.sold AS sold \
     FROM Items I \
     JOIN Categories C ON I.category_id = C.category_id \
-    JOIN Brands B ON I.brand_id = B.brand_id;"
+    JOIN Brands B ON I.brand_id = B.brand_id \
+    ORDER BY date DESC;")
 fruits_data = db_module.execute_all(sql_item)
 sql_category = "SELECT category_id, name FROM Categories;"
 category_data = db_module.execute_all(sql_category)
