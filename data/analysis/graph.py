e5d761d22f969b04a9a38de126539feb5ea536eb
ysolarh <ysolarh@gmail.com>
2024-01-22 23:37:13 +0900

    Fail: pyplot graph - mpld3 -> html
    하긴 했는데 위치 조정이랑 크기조정이 어렵고 업데이트할때마다 가능한지 잘
    모르겠음
    다른 방식 예정

--- data/analysis/graph.py
+++ data/analysis/graph.py
@@ -1,10 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import mpld3
 
-# import sys
+import sys
 
-# sys.path.append(r'fruitsfamily/analysis/db_analysis.py')
+sys.path.append(r'/Users/yyy/Documents/OZ_project/fruitsfamily/')
+
 from data.analysis.db_analysis import DBAnalysis
+
+
 # import matplotlib.font_manager as fm
 
 
@@ -12,20 +16,33 @@ class ShowGraph:
     def __init__(self):
         self.db_analysis = DBAnalysis()
         plt.rc('font', family='AppleGothic')
+        self.fig, self.ax = plt.subplots(1, 1, figsize=(10, 5))
 
     def show_by_category(self) -> None:
         categories = self.db_analysis.get_categories()
         count_category = self.db_analysis.get_count_category()
-        print(count_category)
         x = np.arange(count_category)
 
-        count_by_category = []
+        y = []
         for cate in categories:
             count = self.db_analysis.get_count_by_category(cate)
-            count_by_category.append(count)
-        plt.bar(x, count_by_category, align='center', tick_label=categories)
-        #plt.xticks(x, labels=categories)
+            y.append(count)
+
+        self.ax.bar(x, y, align='center')
+        # self.ax.set_xticks(x, categories)
+        self.ax.set_xticklabels(categories)
+        self.ax.set_xlabel('카테고리')
+        self.ax.set_ylabel('개수')
+        self.ax.set_title('카테고리 별 상품 개수')
         plt.show()
+
+        # plt.bar(x, y, align='center', tick_label=categories)
+        # plt.xticks(x, labels=categories)
+        # plt.show()
+
+        m_html = mpld3.fig_to_html(self.fig, figid='graph-img')
+        with open("graph_show.html", "w") as f:
+            f.write(m_html)
 
 
 # print([f.name for f in fm.fontManager.ttflist])
