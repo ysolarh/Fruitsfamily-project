e972e293f0cfcb93b6bf52f37eb65c75af828778
ysolarh <ysolarh@gmail.com>
2024-01-29 23:27:32 +0900

    Refactor: update index function
    data 변수들 함수 안으로 넣음

--- fruitsfamily/view/main_views.py
+++ fruitsfamily/view/main_views.py
@@ -1,17 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from fruitsfamily.data.database.db_module import DatabaseModule
 from fruitsfamily.data.database.db_query import SQL_ALL, SQL_CATE
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-db_module = DatabaseModule()
-cur = db_module.cur
-fruits_data = db_module.execute_all(SQL_ALL)
-# fruits_data = db_module.execute_all(SQL_SEARCH_PRODUCT, "백팩")
-# fruits_data = db_module.execute_all(SQL_SEARCH_PRODUCT, "NIKE")
-category_data = db_module.execute_all(SQL_CATE)
-
 
 @bp.route('/')
 def index():
+    db_module = DatabaseModule()
+    # cur = db_module.cur
+    category_data = db_module.execute_all(SQL_CATE)
+    fruits_data = db_module.execute_all(SQL_ALL)
+    # fruits_data = db_module.execute_all(SQL_SEARCH_PRODUCT, "백팩")
+    # fruits_data = db_module.execute_all(SQL_SEARCH_PRODUCT, "NIKE")
     return render_template('index.html', data_list=fruits_data, cate_list=category_data)
